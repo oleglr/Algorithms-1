0bf652167183129f1bc8009890b13908ba0a7e8a
Artem <artem.gagolkin22@gmail.com>
2019-07-08 21:49:43 +0300

    add more version

--- generate_array.py
+++ generate_array.py
@@ -3,7 +3,7 @@ from random import randint
 def generate_list(num_elements):
 	ls = []
 
-	for i in range(num_elements):
+	for _ in range(num_elements):
 		ls.append(randint(0, 10))
 
 	return ls
--- insertion sort.py
+++ insertion sort.py
@@ -1,8 +1,5 @@
-from generate_array import *
+from generate_array import generate_list
 
-ls = generate_list(10)
-
-print(ls)
 
 def insertion(ls):
 	for i in range(1, len(ls)):
@@ -17,5 +14,22 @@ def insertion(ls):
 	return ls
 	
 
-sorted_list = insertion(ls)
-print(ls)
+def insertion_acs(ls):
+	for i in range(1, len(ls)):
+		
+		current = ls[i]
+		pos = i
+		while pos > 0 and  ls[pos-1] < current:
+			# swap elements
+			ls[pos] = ls[pos-1]
+			pos -= 1
+		ls[pos] = current
+	return ls
+
+if __name__ == "__main__":
+
+	ls = generate_list(15)
+	print(ls)
+
+	sorted_list = insertion_acs(ls)
+	print(ls)
